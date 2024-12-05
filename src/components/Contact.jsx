73cb4ae78139b47a9ca2ac81eb78c7bf275980a2
nick2601 <59530213+nick2601@users.noreply.github.com>
2024-12-05 08:20:48 +0000

    Project Addition and navigation

---
+++ src/components/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="text-center">
+      <h2 className="text-3xl font-extrabold text-gray-900">Contact Me</h2>
+      <p className="mt-4 text-lg text-gray-500">
+        23 Page Street Swansea SA1 4EY UK
+      </p>
+      <p className="text-lg text-gray-500">
+        <a href="mailto:mule.nikhil@gmail.com">mule.nikhil@gmail.com</a>
+      </p>
+      <p className="text-lg text-gray-500">
+        +44 7424797560
+      </p>
+      <p className="text-lg text-gray-500">
+        <a href="https://www.linkedin.com/in/nikhil-mule2601">LinkedIn</a> | <a href="https://github.com/nick2601">GitHub</a>
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
